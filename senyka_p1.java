4ba8126028d065a09d289aa4d95913df0f7d65e6
senyka <semyon.nikitenko.92@gmail.com>
2017-01-29 22:00:00 +0200

    create Senyka 2

--- senyka_p1.java
+++ senyka_p1.java
@@ -1,6 +1,10 @@
 public class senyka_p1 {
 	public static void main (String [] arg){
 		System.out.println ("Senyka Test Project 1");
+		int a = Integer.valueOf(arg[0]);
+		int b = Integer.valueOf(arg[1]);
+		int sum = a + b;
+		System.out.println ("Summa " + sum);
 	}
 	
 }
